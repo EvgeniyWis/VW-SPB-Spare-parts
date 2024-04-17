74c195dfbc5b36a957cb488da4e6de5a16022bbe
Evgeniy <KworkEvgeniy@yandex.ru>
2024-04-17 20:01:13 +0300

    Изменение main.js.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,15 +2,7 @@
 const navbar__adaptive_menuBurger = document.getElementById("navbar__adaptive_menuBurger");
 const navbar__adaptive_menu = document.getElementById("navbar__adaptive_menu");
 const navbar__adaptive_menu_cross = document.getElementById("navbar__adaptive_menu_cross");
-const sales__tabs__item__all = document.getElementById("sales__tabs__item__all");
-const sales__tabs__item__customers = document.getElementById("sales__tabs__item__customers");
-const sales__tabs__item__service = document.getElementById("sales__tabs__item__service");
-const sales__tabs__item__trade_in = document.getElementById("sales__tabs__item__trade_in");
-const sales__all = document.getElementById("sales__all");
-const sales__customers = document.getElementById("sales__customers");
-const sales__service = document.getElementById("sales__service");
-const sales__trade_in = document.getElementById("sales__trade_in");
-
+const popup__background = document.getElementById("popup__background");
 
 /* Открытие и закрытие бургер меню */
 navbar__adaptive_menuBurger.addEventListener("click", () => {
